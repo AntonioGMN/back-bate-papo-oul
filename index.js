9aaf2316ac772317fa0ae5498b29aa62811ffa20
Antonio <antonio.garciamn@gmail.com>
2022-01-26 14:43:05 -0300

    feat server online

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-import express from "express";
+import express, { json } from "express";
+import cors from "cors";
+
+const serve = express();
+serve.use(json());
+serve.use(cors());
+
+serve.get("/", (req, res) => {
+	res.send("OK");
+});
+
+serve.listen(4000, () => console.log("ouvindo"));
